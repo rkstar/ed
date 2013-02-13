991afc21c2a4f6bf23a7cee613e36f6e6d6b5f61
David Fudge <dfudge@directantidote.com>
2013-02-13 15:16:20 -0500

    removed debug code

--- lib/ThirdParty/PHPExcel/Autoloader.php
+++ lib/ThirdParty/PHPExcel/Autoloader.php
@@ -25,7 +25,7 @@
  * @version    ##VERSION##, ##DATE##
  */
 
-print PHPExcel_Autoloader::Register() ? "autoload worked!" : "no autloading bro";
+PHPExcel_Autoloader::Register();
 //    As we always try to run the autoloader before anything else, we can use it to do a few
 //        simple checks and initialisations
 //PHPExcel_Shared_ZipStreamWrapper::register();
